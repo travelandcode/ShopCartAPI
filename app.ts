7049eb117a7cd536bfc3ea35e9f551ccbd0bc758
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-10-21 22:26:48 -0500

    Setup API Routes

--- app.ts
+++ app.ts
@@ -1,11 +1,40 @@
-import { port } from './src/config/config'
 import express from 'express'
+import passport from 'passport'
+import Config from './src/config/config'
+import authRoutes from './src/routes/auth_routes'
+import { SECRET } from './src/constants/constants'
+import cors from 'cors'
 
 const app = express()
-const PORT = port
+const session = require('express-session')
+const config = new Config()
 
-//Middleware
+
+const PORT = config.PORT
+
+const allowedOrigins = 'http://localhost:3000/';
+
+app.use(cors({
+    origin: function (origin:any, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
+
+//Express setup
+app.use(session({secret: SECRET, resave: true, saveUninitialized: true}))
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//Initialize Passport and Session
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Routes
+app.use(authRoutes)
 
 app.listen(PORT,() =>{
     console.log(`Server is listening on port:${PORT}`)
