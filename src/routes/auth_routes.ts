5f7069337d0c1440ae4d8ddc28572fadb7805395
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-10-23 21:34:25 -0500

    Removed unnecessary imports

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -1,4 +1,4 @@
-import express, { Router, json } from 'express'
+import express from 'express'
 import passport from 'passport'
 import Config from '../config/config'
 import {  User } from '../models/models'
