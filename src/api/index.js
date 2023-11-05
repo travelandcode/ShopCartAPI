934b0c9a5ed25676bdd45ac8f715ae91eb3f5913
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-11-05 09:26:20 -0500

    test vercel deployment

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import logger from '../../src/logs/logger'
+import logger from '../logs/logger'
 import passport from 'passport'
-import Config from '../../src/config/config'
-import authRoutes from '../../src/routes/auth_routes'
-import { SECRET } from '../../src/constants/constants'
+import Config from '../config/config'
+import authRoutes from '../routes/auth_routes'
+import { SECRET } from '../constants/constants'
 import cors from 'cors'
 
 const app = express()
@@ -15,7 +15,7 @@ const PORT = config.PORT
 const allowedOrigins = config.DOMAIN;
 
 app.use(cors({
-    origin: function (origin:any, callback) {
+    origin: function (origin, callback) {
       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
         callback(null, true);
       } else {
