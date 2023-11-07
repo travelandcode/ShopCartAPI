831c5e9378be528da0652c015d4b0155ec0238e7
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-11-07 13:06:50 -0500

    Renamed entry file

--- app.ts
+++ app.ts
@@ -16,7 +16,7 @@ const allowedOrigins = config.DOMAIN;
 
 app.use(cors({
     origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
