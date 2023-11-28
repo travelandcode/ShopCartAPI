78b86d1f813ed03ed35d3edbd0347545cf2dca2c
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-11-27 23:10:36 -0500

    Data Types file added

---
+++ src/models/d.ts
@@ -0,0 +1,18 @@
+import { UserType } from "./enums"
+
+export interface User{
+    type: UserType | string
+    id: string
+    displayName: string
+    email: string
+}
+
+export interface Products{
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    tags: string[],
+    img_src: string,
+    isDeal: boolean
+}
