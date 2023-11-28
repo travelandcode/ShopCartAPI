b655c7ed46411dfc4cecbc654f1e9bd0be021722
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-11-27 23:11:26 -0500

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import logger from '../logs/logger';
 
 dotenv.config();
 
@@ -17,4 +19,27 @@ export default class Config{
     MICROSOFT_CLIENT_SECRET = process.env.MICROSOFT_CLIENT_SECRET || ''
     MICROSOFT_CALLBACK_URL = process.env.MICROSOFT_CALLBACK_URL || ''
     DOMAIN = process.env.DOMAIN || ''
+    MONGODB_URI = process.env.MONGODB_URI || ''
 }
+
+export function connectDB():void {
+    const url = process.env.MONGODB_URI || '';
+    try {
+      mongoose.connect(url,{
+        autoIndex: true,
+        dbName: "shopcart"
+      });
+    } catch (err:any) {
+      logger.error(err.message);
+      process.exit(1);
+    }
+    const dbConnection = mongoose.connection;
+    dbConnection.once("open", (_) => {
+      logger.info(`Successfully Connected to Database`);
+    });
+  
+    dbConnection.on("error", (err) => {
+      logger.error(`${err}`);
+    });
+  }
+  
