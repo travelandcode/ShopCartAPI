cf0df14969510a1eb6cb337ee1e1bd60a77bcd50
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-23 06:13:02 -0500

    Refactored Auth Router

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -1,223 +1,167 @@
-import express, { json } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import passport from 'passport'
 import Config from '../config/config'
-import {  User } from '../models/d'
-import { Strategy as TwitterStrategy } from 'passport-twitter'
-import { Strategy as FacebookStrategy } from 'passport-facebook'
-import { Strategy as MicrosoftStrategy } from 'passport-microsoft'
-import { EMAIL, OPEN_ID, PROFILE, GOOGLE, GOOGLE_AUTH, GOOGLE_AUTH_REDIRECT, FAILURE, GOOGLE_AUTH_SUCCESS, TWITTER_AUTH, TWITTER, TWITTER_AUTH_REDIRECT, TWITTER_AUTH_SUCCESS, NOT_AUTHENTICATED, FACEBOOK_AUTH, FACEBOOK, FACEBOOK_AUTH_REDIRECT, FACEBOOK_AUTH_SUCCESS, AUTH_USER, MICROSOFT_AUTH, MICROSOFT, MICROSOFT_AUTH_REDIRECT, MICROSOFT_AUTH_SUCCESS, LOGOUT, EMPTY_USER } from '../constants/constants'
-import { UserType } from '../models/enums'
+import { User } from '../models/d'
+import { compareSync, hashSync } from 'bcrypt'
+import { Strategy as LocalStrategy } from 'passport-local'
+import { Strategy as GoogleStrategy }  from 'passport-google-oauth20'
+import { GOOGLE, GOOGLE_AUTH, GOOGLE_AUTH_REDIRECT, FAILURE, NOT_AUTHENTICATED,
+LOGOUT, EMPTY_USER, SIGN_UP, LOGIN } from '../constants/constants'
+import nodemailer from 'nodemailer'
 import logger from '../logs/logger'
+import users from '../models/users'
+import tokens from '../models/tokens'
 
 
 const router = express.Router()
-const GoogleStategy = require('passport-google-oidc')
 const config = new Config()
 
-const users: {[key:string]: User} = {}
-let activeUser: User = EMPTY_USER
+//Configure Local Strategy
+passport.use(new LocalStrategy(
+    {usernameField: 'email',
+    passwordField: 'password'},
+    async function (email,password, done) {
+        try{
+            const user = await users.findOne({email: email})
+            if(!user) return done(null, false, {message: "Incorrect Email"})
+            if(!compareSync(password,user.password!)) return done(null, false,{message: "Incorrect Password"})
+            return done(null,user)        
+        }catch(error){
+            logger.error(error)
+        }
+    }
+))
+
 //Configure Google Strategy
-passport.use(new GoogleStategy(
+passport.use(new GoogleStrategy(
     {
         clientID: config.GOOGLE_CLIENT_ID,
         clientSecret: config.GOOGLE_CLIENT_SECRET,
-        callbackURL: config.GOOGLE_CALLBACK_URL
+        callbackURL: config.GOOGLE_CALLBACK_URL,
+        scope: ['profile','email']
     },
-    function(issuer:any,profile:any,done:any) {
-        if(users[profile.id]){
-            return done(null, users[profile.id])
-        }else{
-            console.log(profile)
-            let newUser: User = {
-                id: profile.id,
-                displayName: profile.name.givenName,
-                type: UserType.GOOGLE,
-                email: profile.emails[0].value
+    async function(accessToken,refreshToken,profile,done) {
+        try{
+            let user:any = await users.findOne({email: profile.emails![0].value})
+            if(!user){
+                user = new users({
+                    id: profile.id,
+                    name: profile.name!.givenName,
+                    email: profile.emails![0].value,
+                    isEmailVerified: profile._json.email_verified,
+                    password: ''
+                })
+                await user.save().then(()=>{logger.info('New User Created'
+                )})  
             }
-            users[profile.id] = newUser
-            return done(null, newUser)
+            logger.info(user)
+            return done(null,user!)
+        }catch(error){
+            logger.error(error)
         }
     }
 ))
-
-//Configure Twitter Strategy
-passport.use(new TwitterStrategy(
-    {
-        consumerKey: config.TWITTER_CLIENT_ID,
-        consumerSecret: config.TWITTER_CLIENT_SECRET,
-        callbackURL: config.TWITTER_CALLBACK_URL
-    },
-    (token:any, tokenSecret:any, profile:any, done:any) =>{
-        
-        if(users[profile.id]){
-            return done(null, users[profile.id])
-        }else{
-            let newUser: User = {
-                id: profile.id,
-                displayName: profile.displayName,
-                type: UserType.TWITTER,
-                email: 'defaultemail@gmail.com'
-            }
-            users[profile.id] = newUser
-            return done(null, newUser)
-        }
-    }
-))
-
-//Configure Facebook Strategy
-passport.use(new FacebookStrategy(
-    {
-        clientID: config.FACEBOOK_CLIENT_ID,
-        clientSecret: config.FACEBOOK_CLIENT_SECRET,
-        callbackURL: config.FACEBOOK_CALLBACK_URL
-    },
-    (accessToken, refreshToken, profile, done) => {
-        if(users[profile.id]){
-            return done(null, users[profile.id])
-        }else{
-            let newUser = {
-                id: profile.id,
-                displayName: profile.displayName,
-                type: UserType.FACEBOOK,
-                email: 'defaultemail@gmail.com'
-            }
-            users[profile.id] = newUser
-            return done(null, newUser)
-        }
-    }
-))
-
-//Configure Microsoft Strategy
-passport.use(new MicrosoftStrategy({
-    clientID: config.MICROSOFT_CLIENT_ID,
-    clientSecret: config.MICROSOFT_CLIENT_SECRET,
-    callbackURL: config.MICROSOFT_CALLBACK_URL,
-    scope: ['user.read'],
-    authorizationURL: 'https://login.microsoftonline.com/6e4aab20-70ca-485b-8b19-ba77e89bd5e0/oauth2/v2.0/authorize',
-    tokenURL: 'https://login.microsoftonline.com/6e4aab20-70ca-485b-8b19-ba77e89bd5e0/oauth2/v2.0/token',
-  },
-  (accessToken:any, refreshToken:any, profile:any, done:any) => {
-    logger.info(JSON.stringify(profile))
-    if(users[profile.id]){
-        return done(null, users[profile.id])
-    }else{
-        const email = () =>{
-            profile.emails[0].value
-        }
-        let newUser = {
-            id: profile.id,
-            displayName: profile.name.givenName,
-            type: UserType.MICROSOFT,
-            email: 'defaultemail@gmail.com'
-        }
-        
-        users[profile.id] = newUser
-        return done(null, newUser)
-    }
-  }
-));
 
 //Passport Session Setup
-passport.serializeUser(function(user:any,done:any){
+passport.serializeUser(function(user:any,done){
     done(null, user)
 })
 
-passport.deserializeUser(function(user:any,done:any){
-    done(null,user)
+passport.deserializeUser(async function(user:any,done:any){
+   await users.findOne({ email: user.email })
+  .then((serializedUser) => {
+    // Handle the user data here
+    done(null, serializedUser);
+  })
+  .catch((err) => {
+    // Handle errors here
+    done(err, null);
+  })
 })
 
+router.post(SIGN_UP, async (req,res) => {
+    const generatedNumber = Math.floor(Math.random()* (999 - 100 + 1) + 100)
+    const newId = generatedNumber+''+Date.now()
+    const existingUser = await users.findOne({email: req.body.email})
+    if(existingUser){
+        res.send('User already exists')
+    }
+    else{
+        const user = new users({
+            id: newId,
+            name: req.body.name,
+            password: hashSync(req.body.password, 10),
+            email: req.body.email,
+            isEmailVerified: false
+        })
+        user.save().then(user => logger.info(user));
+        const { formattedCurrentDay, formattedNextDay } = getFormattedDates()
+        const generatedToken = generateToken()
+        const userTokenInfo = new tokens({
+            userId: user.id,
+            token: generatedToken,
+            createdAt: formattedCurrentDay,
+            expiresAt: formattedNextDay
+        })
+        userTokenInfo.save()
+        sendVerificationEmail(user,generatedToken)
+        logger.info('Registering User')
+        res.status(201).send('Please check your email for verification.')
+    }
+})
+
+//Routes for Local Authentication
+router.post("/login", passport.authenticate("local",{successRedirect: "http://localhost:3001/auth/success"}))
+
 //Routes for Google Authentication
-router.get(GOOGLE_AUTH, passport.authenticate(GOOGLE,{ scope: [OPEN_ID, PROFILE, EMAIL] }));
+router.get(GOOGLE_AUTH, passport.authenticate(GOOGLE,{scope: ['profile','email']}))
 
 router.get(GOOGLE_AUTH_REDIRECT, passport.authenticate(GOOGLE, 
         {
             failureRedirect: FAILURE,
-            successRedirect: GOOGLE_AUTH_SUCCESS
+            successRedirect: "/auth/success"
         }
     )
 )
 
-router.get(GOOGLE_AUTH_SUCCESS, (req:any,res) =>{
-    if (req.isAuthenticated()) {
-        logger.info('Google User has been authenticated')
-        activeUser = req.user as User
-        res.redirect('http://localhost:3000/')
-      
-    } else {
-        logger.warn('Google User was not authenticated')
-        res.send(NOT_AUTHENTICATED)
+async function verificationEmailCheck(req:Request,res:Response, next:NextFunction){
+    try{
+    const user:User = req.user as User
+    const existingUserToken = await tokens.findOne({ userId: user.id})
+    if(user && !user.isEmailVerified){
+        if(existingUserToken  && existingUserToken.token){
+            const currentTime = Date.now()
+            const formattedExpireDate = existingUserToken.expiresAt
+            const expiredDate = new Date(formattedExpireDate)
+
+            if(currentTime > expiredDate.getTime()){
+                const newToken = generateToken()
+                const { formattedCurrentDay, formattedNextDay } = getFormattedDates()
+                await tokens.updateOne({userId: user.id},{token: newToken, createdAt:formattedCurrentDay, expiresAt: formattedNextDay})
+                logger.info('User Token has expired')
+                sendVerificationEmail(user, newToken)
+                res.send('Token has expired. New verification email has been sent')
+            }else{
+                res.status(401).send('Check your email and please click on the verification link to login')
+            }
+        }
     }
-})
-
-
-//Routes for Twitter Authentication
-router.get(TWITTER_AUTH, passport.authenticate(TWITTER))
-
-router.get(TWITTER_AUTH_REDIRECT,passport.authenticate(TWITTER,
-        {
-            failureRedirect: FAILURE,
-            successRedirect: TWITTER_AUTH_SUCCESS
-        }
-    )
-)
-
-router.get(TWITTER_AUTH_SUCCESS, (req:any, res) => {
-    if (req.isAuthenticated()) {
-        logger.info('Twitter User has been authenticated')
-        activeUser = req.user as User
-        res.redirect(config.DOMAIN)
-      } else {
-        res.send(NOT_AUTHENTICATED)
-      }
-})
-
-//Routes for Facebook Authentication
-router.get(FACEBOOK_AUTH, passport.authenticate(FACEBOOK))
-
-router.get(FACEBOOK_AUTH_REDIRECT,passport.authenticate(FACEBOOK,
-        {
-            failureRedirect: FAILURE,
-            successRedirect: FACEBOOK_AUTH_SUCCESS
-        }
-    )
-)
-
-router.get(FACEBOOK_AUTH_SUCCESS, (req:any, res) => {
-    if (req.isAuthenticated()) {
-        logger.info('Facebook User has been authenticated')
-        activeUser = req.user as User
-        res.redirect(config.DOMAIN)
-    } else {
-        res.send(NOT_AUTHENTICATED)
+    else{
+       if(user?.isEmailVerified) next()
     }
-})
+    }catch(error){
+        logger.error(error)
+    }
+}
 
-//Routes for Microsoft Authentication
-router.get(MICROSOFT_AUTH, passport.authenticate(MICROSOFT))
-
-router.get(MICROSOFT_AUTH_REDIRECT,passport.authenticate(MICROSOFT,
-        {
-            failureRedirect: FAILURE,
-            successRedirect: MICROSOFT_AUTH_SUCCESS
-        }
-    )
-)
-
-router.get(MICROSOFT_AUTH_SUCCESS, (req:any, res) => {
+//Route for if login was succesful
+router.get("/success",verificationEmailCheck, (req:any,res) =>{
     if (req.isAuthenticated()) {
-        logger.info('Microsoft User has been authenticated')
-        activeUser = req.user as User
         res.redirect('http://localhost:3000/')
     } else {
+        logger.warn('User was not authenticated')
         res.send(NOT_AUTHENTICATED)
     }
-})
-
-//Send Login Information to Client
-router.get(AUTH_USER,(req,res) =>{
-    logger.info('Sending Active User Information to Login')
-    logger.info(JSON.stringify(activeUser))
-    activeUser.displayName === '' ? res.send(null) : res.json(activeUser)
 })
 
 router.get(FAILURE, (req,res) =>{
@@ -228,10 +172,192 @@ router.get(FAILURE, (req,res) =>{
 router.post(LOGOUT, (req,res) =>{
     logger.info('Logging out user')
     req.logout(() => {
-        activeUser = EMPTY_USER
         res.redirect(config.DOMAIN)
         }
     );
 })
 
+router.get('/logout',(req,res)=> {
+    req.logout(() =>{
+        console.log(req.session)
+        res.redirect('http://localhost:3001/auth/login')
+    })
+})
+
+router.get('/verify', async (req, res) => {
+    const  { userToken, userId } = req.query;
+    logger.info(userId)
+    logger.info(userToken)
+    const user = await users.findOne({ id: userId})
+    const token = await tokens.findOne({ userId: user!.id})
+    if (user) {
+      // Mark the user as verified (update your database accordingly)
+      if(token && token.token === userToken){
+        await users.updateOne({id: userId},{isEmailVerified: true})
+        return res.send('Email verification successful. You can now log in.');
+      }
+    }
+    return res.status(400).send('Invalid verification token.');
+}); 
+
+function authenticateUser(req:Request, res:Response, next:NextFunction){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.status(401).send('Unauthorized')
+}
+
+router.get('/user', authenticateUser, (req, res) => {
+    res.status(201).send({user: req.user})
+})
+
+async function sendVerificationEmail(newUser:User, token: String){
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'theshopcartapp@gmail.com',
+            pass: 'dqyg fdka ladm aglx'
+            
+        }
+    })
+
+    const mailOptions = {
+        from: 'smtp.gmail.com',
+        to: newUser.email,
+        subject: 'Email Verification',
+        html:`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://cdn.tailwindcss.com"></script>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Concert+One">
+            <style>
+        
+                body{
+                    width: 100vh;
+                    background-color: #f6f9fc;
+                    height: 100vh;
+                }
+        
+                .email-container{
+                    flex: 1 1 auto;
+                    flex-direction: column; 
+                    width: 600px;
+                    height: auto;
+                    margin: 0 auto;
+                    position: relative;
+                }
+        
+                .email-section{
+                    background-color: #fff;
+                    padding-left: 20px;
+                    padding-right: 20px;
+                    padding-top: 30px;
+                    padding-bottom: 30px;
+                    border-radius: 10px;
+                }
+        
+                .img{
+                    height: 50px;
+                    margin-bottom: 20px;
+                }
+        
+                .verify-link-btn{
+                    border-radius: 10px;
+                    flex: 1 1 auto;
+                    height: 40px;
+                    width: 150px;
+                    margin-bottom: 10px;
+                    background-color: #08ac0a;
+                    border: none;
+                }
+        
+                .email-txt, .email-txt-2{
+                    font-family: "Ubuntu", sans-serif;
+                    font-size: 16px;
+                    margin-bottom: 20px;
+                    color: #808080;
+                }
+        
+                .email-txt-2{
+                    margin-top: 20px;
+                }
+        
+                .verify-txt{
+                    font-family: "Ubuntu", sans-serif;
+                    font-size: 18px;
+                    margin-left: 0px;
+                    margin-right: 0px;
+                    margin-top: 0px;
+                    margin-bottom: 0px;
+                    font-weight: 600;
+                    color: #fff;
+                }
+        
+            </style>
+        </head>
+        <body>
+            <div class="[ email-container ]">
+                <div class="[ email-section ]">
+                    <img class="[ img ]" src="https://i.imgur.com/J7ZurrZ.png" />
+                    <p class="[ email-txt ][ flex-wrap ]">
+                        Thank you for creating a Shopcart account! We're thrilled to have you on board. To kickstart your shopping experience, we kindly ask you to verify your email address.
+                    </p>
+                    <a href ="http://localhost:3001/auth/verify?userId=${newUser.id}&userToken=${token}" ><button class="[ verify-link-btn ]"><p class="[ verify-txt ][ my-auto mx-auto ][ text-[#fff] font-semibold ]">Verify Email</p></button></a>
+                    <p class="[ email-txt-2 ][ flex-wrap ]" >Once you have verified your email, we'll guide through the process of completing your account. Visit our support site if you have questions or need help.</p>
+                </div>
+            </div>
+        </body>
+        </html>`
+    }
+
+    transporter.sendMail(mailOptions, (error,info) =>{
+        if(error) return logger.error(error)
+        logger.info('Email sent: '+ info.response)
+    })
+}
+
+function getFormattedDates(){
+    const currentTimestamp = Date.now()
+    const currentDay = new Date(currentTimestamp)
+    const nextDay = new Date(currentTimestamp)
+    nextDay.setDate(currentDay.getDate() + 1);
+
+    const options: Intl.DateTimeFormatOptions = { 
+        weekday: "long",
+        year: "numeric", 
+        month: "numeric", 
+        day: "numeric", 
+        hour: "numeric", 
+        minute: "numeric", 
+        second: "numeric", 
+        hour12: true 
+    };
+
+    const formattedCurrentDay = new Intl.DateTimeFormat('en-US', options).format(currentDay)
+    const formattedNextDay = new Intl.DateTimeFormat('en-US', options).format(nextDay)
+    return { formattedCurrentDay, formattedNextDay }
+}
+
+function generateToken(){
+    const date = Date.now()
+    const randomLetters  = generateRandomLetters()
+    const token = randomLetters+""+date
+    return token
+}
+
+function generateRandomLetters(){
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let randomLetters = ''
+    for(let i = 0; i < 3; i++){
+        const randomIndex = Math.floor(Math.random() * alphabet.length)
+        randomLetters += alphabet[randomIndex]
+    }
+    return randomLetters
+}
+
+ 
 export default router
