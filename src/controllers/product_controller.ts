d58a9f92c2a3ee6f1b3290ee64382af796fa1a2b
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-23 06:11:28 -0500

    Updated Products Controller

--- src/controllers/product_controller.ts
+++ src/controllers/product_controller.ts
@@ -1,11 +1,11 @@
 import logger from '../logs/logger';
-import { Products } from '../models/d'
+import { Product } from '../models/d'
 import ProductsModel from '../models/products'
 
 
 export const getProducts = async () =>{
     try{
-        const products:Products[] = await ProductsModel.find({}) as Products[]
+        const products:Product[] = await ProductsModel.find({}) as Product[]
         return products
     }catch(error){
         logger.error(error)
