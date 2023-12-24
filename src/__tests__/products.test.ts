b5d743419bd87c4a85b015975950b4bbc9f08d73
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-24 08:30:31 -0500

    feat(product-unit-tests) : Added unit tests

---
+++ src/__tests__/products.test.ts
@@ -0,0 +1,30 @@
+
+describe('Products Service', () =>{
+    test('should add new product', () => {
+        expect.assertions(1)
+    })
+
+    test('should check to see if product already exists', () => {
+        expect.assertions(1)
+    })
+
+    test('should update product', () => {
+        expect.assertions(1)
+    })
+
+    test('should delete product', () => {
+        expect.assertions(1)
+    })
+
+    test('should get all products', () => {
+        expect.assertions(1)
+    })
+
+    test('should get a product', () => {
+        expect.assertions(1)
+    })
+
+    test('should not get anything if product does not exist',() => {
+        expect.assertions(1)
+    })
+})
