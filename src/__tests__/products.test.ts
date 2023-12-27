7f84ad8b1a9b998fc16e97f897d82a3c97c9fb06
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-27 18:34:05 -0500

    feat(product-unit-tests): Added Product Test Cases

--- src/__tests__/products.test.ts
+++ src/__tests__/products.test.ts
@@ -1,30 +1,85 @@
+import { ProductService } from '../controllers/productService';
+import app from '../../app' 
+import { Product } from '../models/d';
 
 describe('Products Service', () =>{
-    test('should add new product', () => {
-        expect.assertions(1)
+
+    let productService:any
+    let updatedInformation:any
+    let server:any
+    let newProduct: Product
+
+    beforeEach(() => {
+        productService = new ProductService();
+        newProduct = {
+            id: 111,
+            name: "Test Product",
+            description: "This is the description for Test Product 1",
+            price: 1000,
+            tags: ["test","product"],
+            img_src: ["https://i.imgur.com/D6aOJWH.png"],
+            isDeal: false
+        }
+        updatedInformation = {
+            description: "This is the new description for Test Product 2",
+            price: 777
+        }
+        server = app.listen(5000)
+        
     })
 
-    test('should check to see if product already exists', () => {
-        expect.assertions(1)
+    afterEach((done) =>{
+        server.close(done)
     })
 
-    test('should update product', () => {
-        expect.assertions(1)
+    test('should get all products',async () => {
+       const products = await productService.getAllProducts()
+       expect(products.length).toBe(24)
     })
 
-    test('should delete product', () => {
-        expect.assertions(1)
+    test('should add new product',async () => {
+        const {createdProduct,created} = await productService.addProduct(newProduct)
+        expect(created).toBe(true)
+        expect(createdProduct.name).toBe("Test Product")
+        expect(createdProduct.id).toBe(111)
+        expect(createdProduct.price).toBe(1000)
     })
 
-    test('should get all products', () => {
-        expect.assertions(1)
+    test('should not create existing product',async () => {
+       const {created} = await productService.addProduct(newProduct)
+       expect(created).toBe(false)
     })
 
-    test('should get a product', () => {
-        expect.assertions(1)
+    test('should get product using id', async() => {
+        const product = await productService.findProductById(newProduct.id)
+        expect(product[0].id).toBe(newProduct.id)
+        expect(product[0].name).toBe(newProduct.name)
+        expect(product[0].price).toBe(newProduct.price)
+        expect(product[0].description).toBe(newProduct.description)
     })
 
-    test('should not get anything if product does not exist',() => {
-        expect.assertions(1)
+    test('should get product using name', async() => {
+        const product = await productService.findProductByName(newProduct.name)
+        expect(product[0].id).toBe(newProduct.id)
+        expect(product[0].name).toBe(newProduct.name)
+        expect(product[0].price).toBe(newProduct.price)
+        expect(product[0].description).toBe(newProduct.description)
+    })
+
+    test('should update product', async() =>{
+       const {updatedProduct,updated} = await productService.updateProduct(newProduct.name,updatedInformation)
+       expect(updated).toBe(true)
+       expect(updatedProduct.description).toBe(updatedInformation.description)
+       expect(updatedProduct.price).toBe(777)
+    })
+
+    test('should delete product',async () => {
+        const {deleted} = await productService.deleteProduct(newProduct.name,updatedInformation)
+        expect(deleted).toBe(true)
+    })
+
+    test('should not get any information if product does not exist',async () => {
+        const product = await productService.findProductById(newProduct.id)
+        expect(product).toEqual([])
     })
 })
