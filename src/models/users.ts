9423bd2960b744c8250f9e15964b9dfdf6c7583c
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-01-23 12:23:42 -0500

    Updated User Schema

--- src/models/users.ts
+++ src/models/users.ts
@@ -8,7 +8,8 @@ const UserSchema = new Schema<User>({
   name: String,
   email: String,
   password: String,
-  isEmailVerified: Boolean
+  isEmailVerified: Boolean,
+  token: String
 },{collection: 'users'});
 
 // Compile model from schema
