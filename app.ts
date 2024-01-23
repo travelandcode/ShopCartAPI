ff28c1f4cd308b821cfa42482f70a6cae32524d2
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-01-23 11:12:59 -0500

    Refactored App.ts

--- app.ts
+++ app.ts
@@ -2,14 +2,16 @@ import Config, { connectDB } from './src/config/config'
 import authRoutes from './src/routes/authRoutes'
 import productRoutes from './src/routes/productRoutes'
 import stripeRoutes from './src/routes/stripeRoutes'
-import { AUTH, CHECKOUT, PRODUCTS} from './src/constants/constants'
+import orderRoutes from './src/routes/orderRoutes'
+import { AUTH, CHECKOUT, ORDERS, PRODUCTS} from './src/utils/constants'
 import cors from 'cors'
 import logger from './src/logs/logger'
 import passport from 'passport'
 import express from 'express'
+import session from 'express-session'
+import { GoogleStrategy, LocalStrategy, deserializeUser, serializeUser } from './src/config/passportConfig'
 
 const app = express()
-const session = require('express-session')
 const config = new Config()
 
 const PORT = config.PORT
@@ -21,8 +23,8 @@ app.use(cors(
   {
     origin: DOMAIN,
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['X-Requested-With', 'content-type'],
     credentials: true,
+    exposedHeaders: ['access-control-allow-credentials']
   }
 ))
 
@@ -35,12 +37,22 @@ app.use(express.urlencoded({extended:true}))
 app.use(passport.initialize())
 app.use(passport.session())
 
+//Passport Strategies
+passport.use(LocalStrategy)
+passport.use(GoogleStrategy)
+
+//Passport Session Setup
+passport.serializeUser(serializeUser)
+passport.deserializeUser(deserializeUser)
+
 //Routes
 app.use(AUTH,authRoutes)
 app.use(PRODUCTS,productRoutes)
 app.use(CHECKOUT,stripeRoutes)
+app.use(ORDERS,orderRoutes)
 
 app.listen(PORT,() =>{
     logger.info(`Server is listening on port:${PORT}`)
 })
+
 export default app
