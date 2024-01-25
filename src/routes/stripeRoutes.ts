a31d0960a623d4014e2860d40986b308b018630d
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-01-25 10:48:14 -0500

    Refactored

--- src/routes/stripeRoutes.ts
+++ src/routes/stripeRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import { EMPTY_PATH } from '../utils/constants'
 import { StripeController } from '../controllers/stripeController'
+import { ProductService } from '../services/productService'
+import { StripeService } from '../services/stripeService'
 
 const router = express.Router()
-const controller = new StripeController()
+const productService = new ProductService()
+const stripeService = new StripeService()
+const controller = new StripeController({productService,stripeService})
 
 router.post(EMPTY_PATH, controller.makeStripePayment)
 
