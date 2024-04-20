892629fc208509ce706adf27d55d7fdf5e2e47c4
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-04-20 15:46:47 -0500

    made a change to cors setup

--- app.ts
+++ app.ts
@@ -25,7 +25,7 @@ app.use(cors(
     origin: DOMAIN,
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
-    exposedHeaders: ['access-control-allow-credentials','Content-Type','Authorization'],
+    allowedHeaders: ['access-control-allow-credentials','Content-Type','Authorization'],
   }
 ))
 
