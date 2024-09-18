0948cb3189816ee133cb7d87de722b29ee4c3aa1
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-09-18 16:15:12 -0500

    added log to sign in

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -49,6 +49,7 @@ export class AuthController{
     successfulGoogleLogin = (req: Request, res: Response) => {
         const user = req.user as User
         if (req.isAuthenticated()) {
+            logger.info("Successfully authenticated user.")
             //if(user.password === "") return res.status(403).send({data:user, message:"Please Enter Password For User"})
             logger.info("Login Attempt was successful")
             res.redirect(`${this.config.DOMAIN}/home`) 
